0bab91e9a0005e72ff5124c579855f16e4713e64
unknown <nguyenduytai945@gmail.com>
2019-06-07 11:39:17 +0700

    add

---
+++ lophinhchunhat.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class lophinhchunhat {
+    double width, height;
+
+    public lophinhchunhat() {
+    }
+
+    public lophinhchunhat(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+
+    public double getPerimeter() {
+        return (this.width + this.height) * 2;
+    }
+
+    public String display() {
+        return "lophinhchunhat{" + "width=" + width + ", height=" + height + "}";
+    }
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the width:");
+            double width = scanner.nextDouble();
+            System.out.print("Enter the height:");
+            double height = scanner.nextDouble();
+            lophinhchunhat rectangle = new lophinhchunhat(width, height);
+            System.out.println("Your Rectangle \n"+ rectangle.display());
+            System.out.println("Perimeter of the Rectangle: "+ rectangle.getPerimeter());
+            System.out.println("Area of the Rectangle: "+ rectangle.getArea());
+    }
+
+}
+
+
